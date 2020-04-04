1f1246238fcc6a6e2bad4cb6574b025eb8882938
Nangman <ckdgh930314@naver.com>
2020-04-04 11:56:47 +0900

    fix

--- src/api/Post/seeFullPost/seeFullPost.js
+++ src/api/Post/seeFullPost/seeFullPost.js
@@ -9,11 +9,14 @@ export default {
       const comments = await prisma
         .post({ id })
         .comments()
+        //fragment를 사용하여 특정한 쿼리문에선 schema의 내용을 부분적으로 불러 올 수 있게 강제할 수 있다.
+        //아래 fragment는 user란 schema에서 username만 불러 올 수 있다.
         .$fragment(COMMENT_FRAGMENT);
       const likeCount = await prisma
         .likesConnection({
           where: { post: { id } }
         })
+        //aggregate 함수 내부의 count 함수로 쿼리문에 입력한 id를 가진 포스트의 좋아요 수를 likeCount에 담는다.
         .aggregate()
         .count();
       return {
